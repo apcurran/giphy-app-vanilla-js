22852d5fc664fc410d3923f923370e74802e71e4
apc <alexcurran.va@gmail.com>
2019-10-29 13:28:35 -0500

    Vanilla JS version of Giphy Search App, also adjusted some input & submit btn styles

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const main = document.querySelector(".results");
+const searchInput = document.getElementById("search");
+const submitBtn = document.querySelector(".form-field-submit");
+const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=XEAkZeR976diLYKcNhMlxn8S9Uvbbfza&rating=pg&q=';
+
+function clearPreviousResults() {
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+}
+
+function createImages(gifs) {
+    for (const gif of gifs) {
+        console.log(gif);
+        const img = document.createElement("img");
+        const gifSrc = gif.images.fixed_height.url;
+        img.src = gifSrc;
+        img.alt = "Gif";
+        img.classList.add("results-gif");
+        main.append(img);
+    }
+}
+
+async function getGifs(event) {
+    event.preventDefault();
+
+    clearPreviousResults();
+
+    const searchInputValue = searchInput.value;
+    const gifyUrl = `${apiUrl}${searchInputValue}`;
+    const response = await fetch(gifyUrl, { mode: "cors" });
+    const data = await response.json();
+    const gifData = await data.data;
+
+    createImages(gifData);
+    searchInput.value = "";
+}
+
+submitBtn.addEventListener("click", getGifs);
