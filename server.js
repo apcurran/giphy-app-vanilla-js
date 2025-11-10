9f720f3a37fbbff65fa390531bf9cc5327cd7899
Alex Curran <alexcurran.va@gmail.com>
2025-11-10 14:26:56 -0600

    Adjust callback fn name, inline `PORT` variable

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import http from "http";
 
-const PORT = process.env.PORT;
-const server = http.createServer(async function cb(req, res) {});
+const server = http.createServer(async function getGiphyData(req, res) {});
 
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}.`);
+server.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}.`);
 });
