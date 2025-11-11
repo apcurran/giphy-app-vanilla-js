c6e5e67103d3ed8beeaa98e76d002bf3045ea0cf
Alex Curran <alexcurran.va@gmail.com>
2025-11-11 14:15:33 -0600

    Major client-side JS refactor for creating more self-contained fns that limit side effects, also improved performance of DOM updates by removing nodes and adding them to DOM in one method: `main.replaceChildren(newNodes)`

--- client/script.js
+++ client/script.js
@@ -4,51 +4,95 @@ const main = document.querySelector(".results");
 const searchInput = document.getElementById("search");
 const form = document.querySelector(".form");
 
-form.addEventListener("submit", getGifs);
+form.addEventListener("submit", handleSubmit);
 
-async function getGifs(event) {
+/**
+ * @param {Event} event - form submission event obj
+ */
+async function handleSubmit(event) {
     event.preventDefault();
-    clearPreviousResults();
 
-    const apiUrl = "http://localhost:5000/api/giphy-proxy";
     const searchInputValue = searchInput.value.trim();
 
     if (!searchInputValue) {
         return;
     }
 
-    const response = await fetch(apiUrl, {
-        method: "POST",
-        body: JSON.stringify({
-            searchValue: searchInputValue,
-        }),
-    });
-    const { data } = await response.json();
-    createImages(data);
-    // clear search input
+    try {
+        const gifs = await fetchGifs(searchInputValue);
+        const videos = gifs.map(transformApiDataToVideoData);
+        renderVideos(videos);
+    } catch (err) {
+        console.error(err);
+    }
+
     searchInput.value = "";
 }
 
-function clearPreviousResults() {
-    main.replaceChildren();
+/**
+ * @param {string} searchValue
+ * @returns {array}
+ */
+async function fetchGifs(searchValue) {
+    const apiUrl = "http://localhost:5000/api/giphy-proxy";
+    const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            searchValue,
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Gif fetch failed: ${response.status}`);
+    }
+
+    const { data } = await response.json();
+
+    return data;
 }
 
-function createImages(gifs) {
-    for (const gif of gifs) {
-        const vid = document.createElement("video");
-        const { alt_text } = gif;
-        const { mp4, width, height } = gif.images.fixed_height;
+/**
+ * @param {object} gifData
+ * @returns {object}
+ */
+function transformApiDataToVideoData(gifData) {
+    const { alt_text } = gifData;
+    const { mp4: src, width, height } = gifData.images.fixed_height;
 
-        vid.src = mp4;
-        vid.ariaLabel = alt_text;
+    return {
+        src,
+        width,
+        height,
+        alt_text,
+    };
+}
+
+/**
+ * @param {object[]} videosData
+ * @returns {void} - side effect of adding video DOM elems to main elem
+ */
+function renderVideos(videosData) {
+    // create all video elems in memory
+    const videoElements = videosData.map(function createVideoElem({
+        src,
+        width,
+        height,
+        alt_text,
+    }) {
+        let vid = document.createElement("video");
+        vid.src = src;
         vid.width = width;
         vid.height = height;
+        vid.ariaLabel = alt_text;
         vid.autoplay = true;
         vid.loop = true;
         vid.muted = true;
         vid.playsInline = true;
         vid.classList.add("results-gif");
 
-        main.append(vid);
-    }
+        return vid;
+    });
+    // batch update to DOM
+    main.replaceChildren(...videoElements);
 }
