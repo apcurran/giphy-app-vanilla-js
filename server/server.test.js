bf5c20ad3cef87e79bd12470f876e4d9dc6ae561
Alex Curran <alexcurran.va@gmail.com>
2025-11-20 16:39:02 -0600

    Test setup for proxy of Giphy API

---
+++ server/server.test.js
@@ -0,0 +1,8 @@
+import assert from "node:assert/strict";
+import { beforeEach, describe, it } from "node:test";
+
+import { MockAgent, setGlobalDispatcher } from "undici";
+
+import { server } from "./server";
+
+describe("Giphy proxy API", () => {});
