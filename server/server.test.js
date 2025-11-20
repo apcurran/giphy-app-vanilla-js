6b9e5a90d560979918a68d6d5a2e04736d7bafac
Alex Curran <alexcurran.va@gmail.com>
2025-11-20 16:54:03 -0600

    Full setup for testing

--- server/server.test.js
+++ server/server.test.js
@@ -1,8 +1,40 @@
 import assert from "node:assert/strict";
-import { beforeEach, describe, it } from "node:test";
+import { after, afterEach, before, beforeEach, describe, it } from "node:test";
 
 import { MockAgent, setGlobalDispatcher } from "undici";
 
-import { server } from "./server";
+import { server } from "./server.js";
 
-describe("Giphy proxy API", () => {});
+const PORT_TEST = 5001;
+const PROXY_URL = `http://localhost:${PORT_TEST}/api/giphy-proxy`;
+
+describe("Giphy proxy API", () => {
+    let listeningServer;
+    let agent;
+
+    before(() => {
+        // mock env variable
+        process.env.GIPHY_KEY = "TEST_KEY";
+        // start the server
+        listeningServer = server.listen(PORT_TEST);
+    });
+
+    beforeEach(() => {
+        agent = new MockAgent();
+        agent.disableNetConnect(); // do not send any real requests
+        setGlobalDispatcher(agent);
+    });
+
+    after(() => {
+        listeningServer.close();
+
+        delete process.env.GIPHY_KEY;
+    });
+
+    afterEach(() => {
+        agent.assertNoPendingInterceptors();
+    });
+
+    // successful requests
+    it("should proxy the search request and Undici mock should intercept Giphy API req", async () => {});
+});
